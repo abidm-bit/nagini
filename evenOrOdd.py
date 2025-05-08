dc8856c605cb7154a9d77b6a83594202dbb69351
abidm-bit <abidmres@gmail.com>
2025-05-08 15:02:00 -0400

    check if a number is even or odd

---
+++ evenOrOdd.py
@@ -0,0 +1,12 @@
+def checkNumber(numb):
+    x = abs(numb)
+    if x%2 ==0: print(str(numb) + " iz even")
+    else : print (str(numb) + " iz odd")
+
+
+
+checkNumber(21)
+checkNumber(26)
+checkNumber(-48)
+checkNumber(47)
+checkNumber(-3)
