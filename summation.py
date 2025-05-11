7029ed7e467d60e47883624d47db2d8a499fb9bf
abidm-bit <abidmres@gmail.com>
2025-05-11 14:56:13 -0400

    sigma notation, summation

---
+++ summation.py
@@ -0,0 +1,14 @@
+# consider a sigma notation for the function 2x^2 +3
+
+def summation():
+    output =0
+    for z in range(1,22):
+        output+= (2*z**2 + 3)
+        print(output) # prints each addition step
+    print()
+    print(output) # prints the sum
+
+
+
+summation()
+
